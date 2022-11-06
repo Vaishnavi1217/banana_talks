57cfef1f822d44ef0fbae671a9dc8e8c3d0d0a7a
Vaishnavi1217 <95132794+Vaishnavi1217@users.noreply.github.com>
2022-11-06 13:34:36 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var translateButton = document.querySelector("#translate-btn")
 var inputText = document.querySelector("#input-txt")
+var outputDiv = document.querySelector("#output-div")
 function click(){
     console.log("Clicked!")
     console.log(inputText.value)
+    outputDiv.innerText = inputText.value
 }
 translateButton.addEventListener("click",click)
    
