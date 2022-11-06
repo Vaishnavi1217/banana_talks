2fe2032cf2ec0a0551d8f7f547721380fb41f83c
Vaishnavi1217 <95132794+Vaishnavi1217@users.noreply.github.com>
2022-11-06 16:08:45 +0530

    javascript wiring done

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-var translateButton = document.querySelector("#translate-btn")
-var inputText = document.querySelector("#input-txt")
-var outputDiv = document.querySelector("#output-div")
-function click(){
-    console.log("Clicked!")
-    console.log(inputText.value)
-    outputDiv.innerText = inputText.value
+var translateButton = document.querySelector("#translate-btn");
+var inputText = document.querySelector("#input-txt");
+var outputDiv = document.querySelector("#output-div");
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+function translation(text){
+    return url + "?" + "text=" + text
 }
+function errorHandler(error){
+    console.log("There is an error", error);
+    alert("There is something wrong with the server.Please try again after some time")
+}
+function click(){
+    // console.log("Clicked!")
+    // console.log(inputText.value)
+    // outputDiv.innerText = inputText.value
+
+    var input = inputText.value;
+
+    fetch(translation(input))
+        .then(response=>response.json())
+        .then(json => {
+            var finalText = json.contents.translated;
+            outputDiv.innerText = finalText;
+            })
+        .catch(errorHandler)
+};
 translateButton.addEventListener("click",click)
    
